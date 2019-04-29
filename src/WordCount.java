ed79842a7ab40b98f6f27c14652ab496219921e7
Simon Redman <simon@ergotech.com>
2019-04-29 14:08:51 -0600

    Add measurement of total packets sent

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,15 +1,10 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.ToolRunner;
 
 public class WordCount {
@@ -27,10 +22,37 @@ public class WordCount {
 				context.write(word, one);
 			}
 		}
-		
+
 		@Override
 		public void cleanup(Context context) {
 			// Called once at the end of the task. Use to output any keys which we were waiting on
+		}
+	}
+
+	public static class WordCountCombiner extends Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
+
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+
+			// TODO: Broadcast here
+			locallyMappedValues.put(key.toString(), sum);
+
+			// Record the fact that we "sent" a packet
+			context.getCounter(WordCountDriver.COMMUNICATION_LOAD_COUNTER.PACKETS_SENT).increment(1);
+		}
+
+		@Override
+		public void cleanup(Context context) {
+			// TODO: Output any potentially-encode-able messages which were not actually encoded
+			return;
 		}
 	}
 
@@ -50,7 +72,10 @@ public class WordCount {
 	}
 	
 	public static void main (String[] args) throws Exception {
-		WordCountDriver driver = new WordCountDriver(WordCount.TokenizerMapper.class, WordCount.IntSumReducer.class);
+		WordCountDriver driver = new WordCountDriver(
+				WordCount.TokenizerMapper.class,
+				WordCount.IntSumReducer.class,
+				WordCount.WordCountCombiner.class);
 		int exitCode = ToolRunner.run(driver, args);
 		System.exit(exitCode);
 	}
--- src/WordCountDriver.java
+++ src/WordCountDriver.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -12,27 +13,42 @@ import org.apache.hadoop.util.Tool;
 
 public class WordCountDriver extends Configured implements Tool {
 	
-	public Class <? extends Mapper> mapper;
-	public Class <? extends Reducer> reducer;
-	public Class <? extends Reducer> combiner;
-	
-	WordCountDriver(Class<? extends Mapper> mapper, Class<? extends Reducer> reducer) {
+	public static enum COMMUNICATION_LOAD_COUNTER {
+		PACKETS_SENT,
+	}
+
+	public Class <? extends Mapper<Object, Text, Text, IntWritable>> mapper;
+	public Class <? extends Reducer<Text, IntWritable, Text, IntWritable>> reducer;
+
+	// Combiner runs a reduce-like operation to collect the outputs of the map operation
+	// In this phase, examine how the file is split and "broadcast" it to all nodes
+	public Class <? extends Reducer<Text, IntWritable, Text, IntWritable>> combiner;
+
+	// The partitioner assigns keys to reducer nodes. In this phase, sort non-broadcast keys
+	// normally, but make sure to send "broadcast" packets to their specified location
+	public Class <? extends Partitioner<Text, IntWritable>> partitioner;
+
+	WordCountDriver(
+			Class<? extends Mapper<Object, Text, Text, IntWritable>> mapper,
+			Class<? extends Reducer<Text, IntWritable, Text, IntWritable>> reducer,
+			Class<? extends Reducer<Text, IntWritable, Text, IntWritable>> combiner) {
 		this.mapper = mapper;
 		this.reducer = reducer;
-		this.combiner = reducer;
+		this.combiner = combiner;
 	}
 
 	@Override
 	public int run(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf, "word count");
+
 		job.setJarByClass(WordCount.class);
 		job.setMapperClass(mapper);
 		job.setCombinerClass(reducer);
 		job.setReducerClass(combiner);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
-		
+
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
