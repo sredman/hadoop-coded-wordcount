c31ab58f9a3046fad3a6b4bfff19d67ea2387bf6
Simon Redman <simon@ergotech.com>
2019-04-29 12:18:16 -0600

    Allow running WordCount standalone

--- src/WordCount.java
+++ src/WordCount.java
@@ -10,6 +10,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.ToolRunner;
 
 public class WordCount {
 
@@ -25,6 +26,11 @@ public class WordCount {
 				word.set(itr.nextToken());
 				context.write(word, one);
 			}
+		}
+		
+		@Override
+		public void cleanup(Context context) {
+			// Called once at the end of the task. Use to output any keys which we were waiting on
 		}
 	}
 
@@ -42,18 +48,10 @@ public class WordCount {
 			context.write(key, result);
 		}
 	}
-
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		Job job = Job.getInstance(conf, "word count");
-		job.setJarByClass(WordCount.class);
-		job.setMapperClass(TokenizerMapper.class);
-		job.setCombinerClass(IntSumReducer.class);
-		job.setReducerClass(IntSumReducer.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	
+	public static void main (String[] args) throws Exception {
+		WordCountDriver driver = new WordCountDriver(WordCount.TokenizerMapper.class, WordCount.IntSumReducer.class);
+		int exitCode = ToolRunner.run(driver, args);
+		System.exit(exitCode);
 	}
 }
---
+++ src/WordCountDriver.java
@@ -0,0 +1,42 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+
+public class WordCountDriver extends Configured implements Tool {
+	
+	public Class <? extends Mapper> mapper;
+	public Class <? extends Reducer> reducer;
+	public Class <? extends Reducer> combiner;
+	
+	WordCountDriver(Class<? extends Mapper> mapper, Class<? extends Reducer> reducer) {
+		this.mapper = mapper;
+		this.reducer = reducer;
+		this.combiner = reducer;
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "word count");
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(mapper);
+		job.setCombinerClass(reducer);
+		job.setReducerClass(combiner);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		boolean success = job.waitForCompletion(true);
+		return success ? 0 : 1;
+	}
+}
