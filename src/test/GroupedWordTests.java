4044a0bdff59d4d8cd310af45243b6c4c06818a5
Simon Redman <simon@ergotech.com>
2019-04-30 17:35:55 -0600

    Test GroupedWord HashCode

--- src/test/GroupedWordTests.java
+++ src/test/GroupedWordTests.java
@@ -11,6 +11,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
@@ -53,5 +56,26 @@ class GroupedWordTests {
 
 		assertEquals(word, copy);
 		assertArrayEquals(word.getLocations(), copy.getLocations());
+	}
+
+	@Test
+	void hashCodeTest() {
+		Random rand = new Random();
+		Set<Integer> seenCodes = new HashSet<Integer>();
+
+		for (int index = 0; index < 10000; index++) {
+			String loc1 = Integer.toString(rand.nextInt());
+			String loc2 = Integer.toString(rand.nextInt());
+			String word = Integer.toString(rand.nextInt());
+
+			GroupedWord test = new GroupedWord();
+			test.set(new String[] {loc1, loc2}, word);
+
+			int hashCode = test.hashCode();
+			if (seenCodes.contains(hashCode)) {
+				fail("Duplicate hash after " + index + " attempts? That seems improbable!");
+			}
+			seenCodes.add(hashCode);
+		}
 	}
 }
